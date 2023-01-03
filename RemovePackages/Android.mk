75c8ba5099e04288a3215bb58d4380c54e3f9887
kssrao13882 <kssrao13882@gmail.com>
2023-01-03 20:28:35 +0530

    RMX1971: Update Runtime Apps Nuke List
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,45 +5,13 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    AmbientSensePrebuilt \
-    arcore \
-    CalendarGooglePrebuilt \
     Chrome \
     Chrome-Stub \
-    ConnMO \
-    ConnMetrics \
-    DCMO \
-    DevicePolicyPrebuilt \
-    DMService \
     Drive \
-    GCS \
-    GoogleTTS \
     Maps \
-    MyVerizonServices \
-    OBDM_Permissions \
-    obdm_stub \
-    OdadPrebuilt \
-    OemDmTrigger \
-    OPScreenRecord \
-    Ornament \
     Photos \
-    PixelBuds \
-    PixelLiveWallpaperPrebuilt \
     PrebuiltGmail \
-    RecorderPrebuilt \
-    SafetyHubPrebuilt \
-    SCONE \
-    ScribePrebuilt \
-    Showcase \
-    SoundAmplifierPrebuilt \
-    SprintDM \
-    SprintHM \
-    Tycho \
-    USCCDM \
-    Via \
     Videos \
-    VZWAPNLib \
-    VzwOmaTrigger \
     YouTube \
     YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
