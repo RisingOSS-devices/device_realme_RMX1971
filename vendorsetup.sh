662136d228b52ce3d0c3a97e2965d6c6d34ce66d
kssrao13882 <kssrao13882@gmail.com>
2023-05-14 12:33:38 +0530

    RMX1971: Update Vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,11 +2,11 @@ echo 'Starting to clone stuffs needed for your device'
 
 echo 'Cloning Device tree'
 # Device Tree
-rm -rf device/realme/RMX1971 && git clone https://github.com/risingOSS-Devices/device_realme_RMX1971 -b rising device/realme/RMX1971
+rm -rf device/realme/RMX1971 && git clone https://github.com/risingOSS-Devices/device_realme_RMX1971 -b 13 device/realme/RMX1971
 
 echo 'Cloning Common tree'
 # CommonTree
-rm -rf device/realme/sdm710-common && git clone https://github.com/risingOSS-Devices/device_realme_sdm710-common -b 13-op device/realme/sdm710-common
+rm -rf device/realme/sdm710-common && git clone https://github.com/risingOSS-Devices/device_realme_sdm710-common -b 13 device/realme/sdm710-common
 
 echo 'Cloning Kernel tree'
 # Kernel Tree
@@ -18,7 +18,7 @@ rm -rf vendor/realme/RMX1971 && git clone https://gitlab.com/dotos-devices/vendo
 
 echo 'Cloning Vendor Common tree'
 # Vendor Common Tree
-rm -rf vendor/realme/sdm710-common && git clone https://github.com/kssrao13882/vendor_realme_sdm710-common-RD -b 13-op vendor/realme/sdm710-common
+rm -rf vendor/realme/sdm710-common && git clone https://gitlab.com/kssrao13882/vendor_realme_sdm710-common.git -b 13.1 vendor/realme/sdm710-common
 
 echo 'Cloning Proton Clang'
 rm -rf prebuilts/clang/host/linux-x86/clang-proton;git clone --depth=1 https://github.com/kdrag0n/proton-clang  prebuilts/clang/host/linux-x86/clang-proton
